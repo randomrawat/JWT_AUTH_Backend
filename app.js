727b17c7a4db6b55b57e2da3fac03918b56170e4
randomrawat <shubhamrawat8249@gmail.com>
2025-06-07 21:19:39 +0530

    Change the frontend url

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ const cors = require('cors')
 const bodyParser = require('body-parser')
 require('dotenv').config()
 const db = require('./Models/db')
-const PORT = process.env.PORT 
+const PORT = process.env.PORT
 const AuthRouter = require('./Routes/AuthRouter')
 const productRouter = require('./Routes/productRouter')
 
 app.use(bodyParser.json())
 app.use(cors({
-     origin: ['http://localhost:3000', 'http://127.0.0.1:3000'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true 
+  origin: ['https://jwt-auth-frontend.vercel.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true
 }))
-app.use('/auth',AuthRouter)
+app.use('/auth', AuthRouter)
 app.use('/products', productRouter)
 
-app.listen(PORT, ()=> console.log(`Server is running on PORT ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running on PORT ${PORT}`))
